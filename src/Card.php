d60f9270ee3e65c5f16f5c245da9f48f8814b029
Michael Perks <michael.perks@live.co.uk>
2020-10-07 17:49:07 +0100

    Card instantiation and tests

--- src/Card.php
+++ src/Card.php
@@ -2,10 +2,245 @@
 
 namespace SimpleHacker\PHPoker;
 
+use Exception;
+
 class Card
 {
-    public function __construct()
+    /**
+     * The card's value
+     * 
+     * @var mixed
+     */
+    protected $value;
+
+    /**
+     * The card's suit
+     *
+     * @var mixed
+     */
+    protected $suit;
+
+    /**
+     * An array of valid card values with ranking values
+     *
+     * @var array
+     */
+    protected $values = [
+        1 => ['short_value' => 'a', 'value' => 'ace'],
+        2 => ['short_value' => 2, 'value' => 'two'],
+        3 => ['short_value' => 3, 'value' => 'three'],
+        4 => ['short_value' => 4, 'value' => 'four'],
+        5 => ['short_value' => 5, 'value' => 'five'],
+        6 => ['short_value' => 6, 'value' => 'six'],
+        7 => ['short_value' => 7, 'value' => 'seven'],
+        8 => ['short_value' => 8, 'value' => 'eight'],
+        9 => ['short_value' => 9, 'value' => 'nine'],
+        10 => ['short_value' => 't', 'value' => 'ten'],
+        11 => ['short_value' => 'j', 'value' => 'jack'],
+        12 => ['short_value' => 'q', 'value' => 'queen'],
+        13 => ['short_value' => 'k', 'value' => 'king'],
+    ];
+
+    /**
+     * An array of valid card suits with ranking values
+     *
+     * @var array
+     */
+    protected $suits = [
+        1 => ['short_suit' => 'c', 'suit' => 'club'],
+        2 => ['short_suit' => 'd', 'suit' => 'diamond'],
+        3 => ['short_suit' => 'h', 'suit' => 'heart'],
+        4 => ['short_suit' => 's', 'suit' => 'spade'],
+    ];
+
+    /**
+     * Instantiate card with value and suit
+     *
+     * @param string|integer $value
+     * @param string|integer $suit
+     */
+    public function __construct($value, $suit)
     {
+        $this->setValue($value);
+        $this->setSuit($suit);
+    }
+
+    /**
+    * Set the card's value
+    *
+    * @param string|integer $value
+    */
+    private function setValue($value)
+    {
+        if (! $this->isValidValue($value)) {
+            throw new Exception('Invalid card value');
+        }
+
+        if (is_int($value)) {
+            $this->value = $value;
+            return;
+        }
+
+        $value = strtolower($value);
+
+        if (strlen($value) === 1) {
+            $this->value = array_search($value, array_column($this->values, 'short_value')) + 1;
+        } else {
+            $this->value = array_search($value, array_column($this->values, 'value')) + 1;
+        }
+    }
+
+    /**
+    * Set the card's suit
+    *
+    * @param string|integer $suit
+    */
+    private function setSuit($suit)
+    {
+        if (! $this->isValidSuit($suit)) {
+            throw new Exception('Invalid card suit');
+        }
         
+        if (is_int($suit)) {
+            $this->suit = $suit;
+            return;
+        }
+        
+        $suit = strtolower($suit);
+
+        // If suit is one character
+        if (strlen($suit) === 1) {
+            // Find index of short suit.
+            $this->suit = array_search($suit, array_column($this->suits, 'short_suit')) + 1;
+        } else {
+            $suit = rtrim($suit, 's');
+            $this->suit = array_search($suit, array_column($this->suits, 'suit')) + 1;
+        }
+    }
+
+    /**
+    * Check if valid value of card when instantiating
+    * 
+    * @param mixed $value
+    * @return boolean
+    */
+    private function isValidValue($value): Bool
+    {
+        if (is_int($value)) {
+            return ($value > 0 && $value < 14);
+        }
+
+        $value = strtolower($value);
+
+        if (strlen($value) === 1) {
+            return array_search($value, array_column($this->values, 'short_value')) + 1;
+        } else {
+            return array_search($value, array_column($this->values, 'value')) + 1;
+        }
+    }
+
+    /**
+    * Check if valid suit of card when instantiating
+    * 
+    * @param mixed $suit
+    * @return boolean
+    */
+    private function isValidSuit($suit): Bool
+    {
+        if (is_int($suit)) {
+            return ($suit > 0 && $suit < 14);
+        }
+
+        $suit = strtolower($suit);
+
+        // Valid suits
+        if (strlen($suit) === 1) {
+            return in_array($suit, array_column($this->suits, 'short_suit'));
+        } else {
+            $suit = rtrim($suit, 's');
+            return in_array($suit, array_column($this->suits, 'suit'));
+        }
+    }
+
+    /**
+    * Returns the card's value
+    * 
+    * @return string
+    */
+    public function getValue(): String
+    {
+        return ucwords($this->values[$this->value]['value']);
+    }
+
+    /**
+    * Returns the card's value
+    * 
+    * @return string
+    */
+    public function getShortValue(): String
+    {
+        return ucwords($this->values[$this->value]['short_value']);
+    }
+
+    /**
+    * Returns the card's value
+    * 
+    * @return string
+    */
+    public function getValueRank(): String
+    {
+        return $this->value;
+    }
+
+    /**
+    * Returns the card's suit
+    * 
+    * @return string
+    */
+    public function getSuit(): String
+    {
+        return ucwords($this->suits[$this->suit]['suit']) . 's';
+    }
+
+    /**
+    * Returns the card's short suit
+    * 
+    * @return string
+    */
+    public function getShortSuit(): String
+    {
+        return $this->suits[$this->suit]['short_suit'];
+    }
+
+    /**
+    * Returns the card's suit rank value
+    * 
+    * @return integer
+    */
+    public function getSuitRank(): Int
+    {
+        return $this->suit;
+    }
+
+    /**
+    * Returns the card's full description
+    * e.g. Four of Hearts, King of Spades
+    * 
+    * @return string
+    */
+    public function getDescription(): String
+    {
+        return $this->getValue() . ' of ' . $this->getSuit();
+    }
+
+    /**
+    * Returns the card's short description
+    * e.g. 4h, Kc, 3s
+    * 
+    * @return string
+    */
+    public function getShortDescription(): String
+    {
+        return $this->getShortValue() . $this->getShortSuit();
     }
 }
--- tests/CardTest.php
+++ tests/CardTest.php
@@ -2,13 +2,132 @@
 
 namespace SimpleHacker\PHPoker\Tests;
 
+use SimpleHacker\PHPoker\Card;
+
 use PHPUnit\Framework\TestCase;
 
 class CardTest extends TestCase
 {
-    /** @test */
-    public function it_has_a_value()
+    /**
+    * @test
+    * @dataProvider values
+    */
+    public function a_card_can_be_instantiated_with_valid_values($testValue, $value, $shortValue, $valueRank)
     {
-        $this->assertTrue(true);
+        $card = new Card($testValue, 'spades');
+
+        $this->assertEquals($value, $card->getValue());
+        $this->assertEquals($shortValue, $card->getShortValue());
+        $this->assertEquals($valueRank, $card->getValueRank());
+    }
+
+    /**
+    * @test
+    * @dataProvider suits
+    */
+    public function a_card_can_be_instantiated_with_valid_suits($testSuit, $suit, $shortSuit, $suitRank)
+    {
+        $card = new Card('A', $testSuit);
+
+        $this->assertEquals($suit, $card->getSuit());
+        $this->assertEquals($shortSuit, $card->getShortSuit());
+        $this->assertEquals($suitRank, $card->getSuitRank());
+    }
+
+    /**
+    * @test
+    * @dataProvider descriptions
+    */
+    public function a_card_has_a_description($value, $suit, $description)
+    {
+        $card = new Card($value, $suit);
+
+        $this->assertEquals($card->getDescription(), $description);
+    }
+
+    /**
+    * @test
+    * @dataProvider shortDescriptions
+    */
+    public function a_card_has_a_short_description($value, $suit, $description)
+    {
+        $card = new Card($value, $suit);
+
+        $this->assertEquals($card->getShortDescription(), $description);
+    }
+
+    public function values()
+    {
+        return [
+            [1, 'Ace', 'A', 1],
+            ['A', 'Ace', 'A', 1],
+            ['Ace', 'Ace', 'A', 1],
+            [2, 'Two', 2, 2],
+            ['Two', 'Two', 2, 2],
+            [3, 'Three', 3, 3],
+            ['Three', 'Three', 3, 3],
+            [4, 'Four', 4, 4],
+            ['Four', 'Four', 4, 4],
+            [5, 'Five', 5, 5],
+            ['Five', 'Five', 5, 5],
+            [6, 'Six', 6, 6],
+            ['Six', 'Six', 6, 6],
+            [7, 'Seven', 7, 7],
+            ['Seven', 'Seven', 7, 7],
+            [8, 'Eight', 8, 8],
+            ['Eight', 'Eight', 8, 8],
+            [9, 'Nine', 9, 9],
+            ['Nine', 'Nine', 9, 9],
+            [10, 'Ten', 'T', 10],
+            ['T', 'Ten', 'T', 10],
+            ['Ten', 'Ten', 'T', 10],
+            [11, 'Jack', 'J', 11],
+            ['J', 'Jack', 'J', 11],
+            ['Jack', 'Jack', 'J', 11],
+            [12, 'Queen', 'Q', 12],
+            ['Q', 'Queen', 'Q', 12],
+            ['Queen', 'Queen', 'Q', 12],
+            [13, 'King', 'K', 13],
+            ['K', 'King', 'K', 13],
+            ['King', 'King', 'K', 13],
+        ];
+    }
+
+    public function suits()
+    {
+        return [
+            ['c', 'Clubs', 'c', 1],
+            ['club', 'Clubs', 'c', 1],
+            ['clubs', 'Clubs', 'c', 1],
+            ['d', 'Diamonds', 'd', 2],
+            ['diamond', 'Diamonds', 'd', 2],
+            ['diamonds', 'Diamonds', 'd', 2],
+            ['h', 'Hearts', 'h', 3],
+            ['heart', 'Hearts', 'h', 3],
+            ['hearts', 'Hearts', 'h', 3],
+            ['s', 'Spades', 's', 4],
+            ['spade', 'Spades', 's', 4],
+            ['spades', 'Spades', 's', 4],
+        ];
+    }
+
+    public function descriptions()
+    {
+        return [
+            ['ace', 'spade', 'Ace of Spades'],
+            ['Jack', 'diamonds', 'Jack of Diamonds'],
+            [4, 'Hearts', 'Four of Hearts'],
+            ['T', 'clubs', 'Ten of Clubs'],
+        ];
+    }
+
+    public function shortDescriptions()
+    {
+        return [
+            ['ace', 'spade', 'As'],
+            ['Jack', 'diamonds', 'Jd'],
+            [4, 'Hearts', '4h'],
+            ['T', 'clubs', 'Tc'],
+        ];
     }
 }
