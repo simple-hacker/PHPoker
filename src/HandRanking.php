dc27127a42d9e098064514e6f6be57b20d758fc6
Michael Perks <michael.perks@live.co.uk>
2020-10-30 11:32:35 +0000

    Hand ranking flushes

--- src/HandRanking.php
+++ src/HandRanking.php
@@ -2,13 +2,33 @@
 
 namespace simplehacker\PHPoker;
 
+use phpDocumentor\Reflection\Types\Boolean;
 use simplehacker\PHPoker\Card;
 use simplehacker\PHPoker\Exceptions\InvalidCardException;
 use simplehacker\PHPoker\Exceptions\InvalidHandRankingException;
 
 class HandRanking
 {
+    /**
+     * The cards given for the hand ranking
+     * 
+     * @var array
+     */
     protected $cards = [];
+
+    /**
+     * The cards sorted grouped and sorted by value
+     * 
+     * @var array
+     */
+    protected $valueHistogram = [];
+
+    /**
+     * The cards sorted grouped and sorted by suit
+     * 
+     * @var array
+     */
+    protected $suitHistogram = [];
 
     /**
     * Instantiate the Hand Ranking class
@@ -43,12 +63,14 @@ class HandRanking
             throw new InvalidHandRankingException("Duplicate cards given: $invalidCards");
         }
 
-        // Throw error if less than five cards given
-        // Because at least five are needed to determine hand rank
+        // Throw error if less than five cards given because at least five are needed to determine hand rank
         // More than five valid because we determine best five cards out of x provided
         if (count($this->cards) < 5) {
             throw new InvalidHandRankingException('Need at least 5 cards to determine hand ranking');
         }
+
+        $this->valueHistogram = $this->computeValueHistogram();
+        $this->suitHistogram = $this->computeSuitHistogram();
     }
 
     /**
@@ -59,5 +81,82 @@ class HandRanking
     public function getCards(): Array
     {
         return $this->cards;
+    }
+
+    /**
+    * Returns the protected array of cards grouped and sorted by value
+    *
+    * @return array
+    */
+
+    public function getValueHistogram(): Array
+    {
+        return $this->valueHistogram;
+    }
+
+    /**
+    * Returns the protected array of cards grouped and sorted by suits
+    *
+    * @return array
+    */
+    public function getSuitHistogram(): Array
+    {
+        return $this->suitHistogram;
+    }
+
+    /**
+    * Group and sort the cards according to their values
+    * 
+    * @return array
+    */
+    private function computeValueHistogram(): Array
+    {
+        return [];
+    }
+
+    /**
+    * Group and sort the cards according to their suits
+    * Group the cards in to suits.
+    * Sort all suits by the count of cards in each suit.
+    * Sort cards in each suit according to their value.
+    * This is so if there is a suit with 5 cards, then a flush exists
+    * and we want the flush sorted according to their value so we know the highest flush value
+    * 
+    * @return array
+    */
+    private function computeSuitHistogram(): Array
+    {
+        $suits = [];
+
+        // Group all cards in to suits
+        foreach ($this->cards as $card) {
+            $suits[$card->getSuitRank()][] = $card;
+        }
+
+        // Sort suits by count of cards in each suits.
+        // uasort is user defined sorting function which maintains keys which are numerical value of suit
+        uasort($suits, fn($suit1, $suit2) => count($suit2) <=> count($suit1));
+
+        // Sort cards of each suit according to their value
+        foreach($suits as $index => $suit) {
+            // Create copy of array to sort
+            $sortedValues = $suit;
+            usort($sortedValues, fn($card1, $card2) => $card2->getValueRank() <=> $card1->getValueRank());
+            // Replace suit array with sortedArray at index
+            $suits[$index] = $sortedValues;
+        }
+
+        return $suits;
+    }
+
+    /**
+    * Returns if the hand ranking is a flush
+    * Determined if the count of the first element of suit histogram is five or greater
+    * 
+    * @return bool
+    */
+    public function isFlush(): Bool
+    {
+        return count(reset($this->suitHistogram)) >= 5;
     }
 }
--- tests/HandRankingTest.php
+++ tests/HandRankingTest.php
@@ -46,4 +46,45 @@ class HandRankingTest extends TestCase
         $cards = 'Ac2h3s4d3s';
         $hand = new HandRanking($cards);
     }
+
+    /** @test */
+    public function cards_are_grouped_and_sorted_by_suit()
+    {
+        // 3 hearts, 1 diamond and 1 spade.
+        $AceH = new Card('Ah');
+        $TwoH = new Card('2h');
+        $ThreeH = new Card('3h');
+        $FourD = new Card('4d');
+        $FiveS = new Card('5s');
+
+        $hand = new HandRanking([$FourD, $FiveS, $ThreeH, $AceH, $TwoH]);
+
+        $expected = [
+            3 => [$ThreeH, $TwoH, $AceH],
+            4 => [$FiveS],
+            2 => [$FourD]
+        ];
+
+        // Note if this fails in the future it could be to do with Ace value rank being 1 instead of 14
+        $this->assertEquals($expected, $hand->getSuitHistogram());
+    }
+
+    /**
+     * @test
+     * @dataProvider flushes
+    */
+    public function hand_ranking_is_a_flush($hand, $isFlush)
+    {
+        $hand = new HandRanking($hand);
+
+        $this->assertEquals($hand->isFlush(), $isFlush);
+    }
+
+    public function flushes() {
+        return [
+            ['7h3s8h2hKh4dTh', true],
+            ['3s4s5s6sKs', true],
+            ['2d3s4s8c9c', false],
+        ];
+    } 
 }
